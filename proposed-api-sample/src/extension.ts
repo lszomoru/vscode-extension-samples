79fa43494967889f1791d1ece9ac2eb2beb275de
Ladislau Szomoru <lszomoru@microsoft.com>
2021-01-25 21:26:13 +0100

    Proposed api sample

--- proposed-api-sample/src/extension.ts
+++ proposed-api-sample/src/extension.ts
@@ -8,9 +8,19 @@ export function activate(context: vscode.ExtensionContext) {
 	 * Proposed API as defined in vscode.proposed.d.ts.
 	 */
 
-	const disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World!');
+	const command1 = vscode.commands.registerCommand('extension.getWorkspaceTrustState', () => {
+		const trustState = vscode.workspace.trustState;
+		vscode.window.showInformationMessage(`Workspace trust state: ${trustState}`);
 	});
 
-	context.subscriptions.push(disposable);
+	const command2 = vscode.commands.registerCommand('extension.requireWorkspaceTrust', async () => {
+		const trustState = await vscode.workspace.requireWorkspaceTrust('In order to complete this action you must trust the contents of this workpace');
+		vscode.window.showInformationMessage(`Workspace trust state: ${trustState}`);
+	});
+
+	vscode.workspace.onDidChangeWorkspaceTrustState(state => {
+		vscode.window.showInformationMessage(`Workspace trust state changed. Previous state: ${state.previousTrustState}, current state: ${state.currentTrustState}`);
+	});
+
+	context.subscriptions.push(...[command1, command2]);
 }
